e48cd11d33003772da4add3d15396755e332385a
mITy.John <74149552+janvanwassenhove@users.noreply.github.com>
2026-09-06 19:39:37 +0200

    fix: defer WebGL until race start

--- dist/src/assets.js
+++ dist/src/assets.js
@@ -1,7 +1,7 @@
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
-import {createVehicle} from './models.js?v=7';
-import {createCreature,colorSpecies} from './creatures.js?v=7';
-import {getHabitat,SPECIES} from './habitats.js?v=7';
+import {createVehicle} from './models.js?v=8';
+import {createCreature,colorSpecies} from './creatures.js?v=8';
+import {getHabitat,SPECIES} from './habitats.js?v=8';
 import {mergeGeometries} from 'three/addons/utils/BufferGeometryUtils.js';
 const cache=new Map(),loading=new Map(),preparedAnimals=new Map();
 export async function preloadModels(key='aquarium'){
